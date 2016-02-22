74aac9700ce8e7eb9269505e539185ec4344b0ae
Jonah Hontos <jonahhontos@gmail.com>
2016-02-22 14:39:36 -0800

    edit instructor

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -7,17 +7,38 @@ class InstructorsController < ApplicationController
   end
 
   def new
+    @instructor = Instructor.new
   end
 
   def create
+    @instructor = Instructor.new(instructor_params)
+
+    if @instructor.save
+      redirect_to instructors_path
+    else
+      redirect_to new_instructor_path
+    end
   end
 
   def edit
+    @instructor = Instructor.find(params[:id])
   end
 
   def update
+    @instructor = Instructor.find(params[:id])
+
+    if @instructor.update_attributes(instructor_params)
+      redirect_to instructors_path
+    else
+      redirect_to edit_instructor_path
+    end
   end
 
   def destroy
+  end
+
+  private
+  def instructor_params
+    params.require(:instructor).permit(:name, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "instructors#index"
 
-  get 'instructors' => "instructors#index"
+  # get 'instructors' => "instructors#index"
+  # get 'instructors/new' => 'instructors#new', as: :new_instructor
+  resources :instructors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
