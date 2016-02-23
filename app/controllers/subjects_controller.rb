33a67d7b0ba5e7afe901b64e954806dbe251c5e6
Jonah Hontos <jonahhontos@gmail.com>
2016-02-23 13:32:56 -0800

    subjects controller

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,23 @@
+class SubjectsController < ApplicationController
+  def index
+    @subjects = Subject.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :subjects
+
   resources :exercises
   get 'exercises/new/:id' => 'exercises#new'
 
