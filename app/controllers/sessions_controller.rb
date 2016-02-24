b7d79db17214bd77cb21d6be363be43e28e22a14
Jonah Hontos <jonahhontos@gmail.com>
2016-02-23 18:14:01 -0800

    instructor + student log in/out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,41 @@
 class SessionsController < ApplicationController
   def new
+    @@type = params[:type]
   end
 
   def create
-    @instructor  = Instructor.find_by({name: params[:name]})
-    if @instructor && @instructor.authenticate(params[:password])
-      session[:instructor_id] = @instructor.id
-      flash[:success] = "Welcome, #{@instructor.name}"
-      redirect_to instructor_path(@instructor)
+    if @@type == "instructor"
+      @instructor  = Instructor.find_by({name: params[:name]})
+      if @instructor && @instructor.authenticate(params[:password])
+        session[:instructor_id] = @instructor.id
+        flash[:success] = "Welcome, #{@instructor.name}"
+        redirect_to instructor_path(@instructor)
+      else
+        flash[:failure] = "Login unsuccessful"
+        redirect_to new_session_path + "?type=instructor"
+      end
+    elsif @@type == "student"
+      @student  = Student.find_by({name: params[:name]})
+      if @student && @student.authenticate(params[:password])
+        session[:student_id] = @student.id
+        flash[:success] = "Welcome, #{@student.name}"
+        redirect_to root_path
+      else
+        flash[:failure] = "Login unsuccessful"
+        redirect_to new_session_path + "?type=student"
+      end
     else
       flash[:failure] = "Login unsuccessful"
-      redirect_to new_session_path
+      redirect_to root_path
     end
   end
 
   def destroy
-    session[:instructor_id] = nil
+    if params[:type] == "instructor"
+      session[:instructor_id] = nil
+    elsif params[:type] == "student"
+      session[:student_id] = nil
+    end
     redirect_to root_path
   end
 end
