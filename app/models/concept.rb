7edef0b1410b682e99e1a7cf193e6bd6533dfdd8
Jonah Hontos <jonahhontos@gmail.com>
2016-02-25 19:08:24 -0800

    dd revise

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,5 +1,5 @@
 class Concept < ActiveRecord::Base
   belongs_to :course
-  has_many :exercises
+  has_many :exercises, dependent: :destroy
   has_and_belongs_to_many :progresses
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,5 @@ class Course < ActiveRecord::Base
   belongs_to :instructor
   belongs_to :subject
   has_and_belongs_to_many :students
-  has_many :concepts
-  dependent: :destroy
+  has_many :concepts, dependent: :destroy
 end
